a0e7feeea19d7bcc818644a091e59307fa3531cf
fsusam <fsusam@gmail.com>
2019-11-14 21:56:05 +0000

    Split the function into reusable functions

--- statistical_data_analysis_tool.py
+++ statistical_data_analysis_tool.py
@@ -5,29 +5,32 @@ Created on Wed Nov 13 21:25:32 2019
 
 @author: fsusam
 """
-# module to calculate standard deviation of data set
+# provides some statistical data analysis methods
 # sample data set [10,8,10,8,8,4]
 
 import math
 
-# how spread out your data is in your sample.    
+# a measure of the amount of variation or dispersion of a set of values
 def standard_deviation(data_set):
-    # Calculating the Standard Deviation    
-    ss=round(math.sqrt(variance(data_set, mean(data_set))), 2)
+    # Calculate the Standard Deviation    
+    ss=round(math.sqrt(variance(data_set)), 2)
     print(f"standard deviation: {ss:.2f}")    
     return ss
 
 
-# calculate the average of the data set
-def mean(data_set):    
+# average of the data set
+def mean(data_set):
+    # Calculate mean        
     mean=sum(data_set)/len(data_set)
     print(f"mean: {mean}")    
     return mean
 
 
-# how far the data in data set is clustered around the mean
-def variance(data_set, mean):
+# how far a set of (random) numbers are spread out from their average value
+def variance(data_set):
+    # call mean function
+    average=mean(data_set)
     # find the Variance    
-    variance=sum([abs((x-mean)**2) for x in data_set]) / (len(data_set)-1)
+    variance=sum([abs((x-average)**2) for x in data_set]) / (len(data_set)-1)
     print(f"variance: {variance:.2f}")    
     return variance
