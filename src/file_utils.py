a86f93d8b729efa2487e1301127ede84cc3a377b
fsusam <fsusam@gmail.com>
2019-11-28 23:52:29 +0000

    format the modules and add docstring

--- src/file_utils.py
+++ src/file_utils.py
@@ -1,187 +1,231 @@
 # Program Name: file_utils
 # Purpose: Read a file and parse by a delimiter, help to create a new data set
 
-
+# Read CSV file line by line and split the lines into columns
 def read_csv(path, delimiter=',', header=True, clean_chars_header=[]):
+    """
+    Read CSV file line by line and split the lines into columns
+
+    Parameters
+    ----------
+    path : str
+        absolute file path. eg: /home/user/workspace/data.csv
+
+    delimiter : str
+        splitter char for lines. Default is ","
+
+    header : bool
+        specify the file has a header. Default is "True"
+
+    clean_chars_header : list
+        it will be removed the chars inside list. Default is "empty list"
+
+    Returns
+    -------
+    list
+        return the dictionary list. eg [{"Name":"test", "LastName":"test"}]
+    """
+    # return list
     result = []
+    # create empty list
     columns = []
+    # flag to check the columns was created
     is_columns_created = False
+    # it will be increment +1 if face any issue during splitting
     split_error_count = 0
+    # Display a sample line which is faced an issue during splitting
     sample_split_error = ''
-    
-    # read the data from the file
+
+    # open the file
     with open(path) as datafile:
-        # for each line in the file
+        # read the file line by line
         for line in datafile:
-            row = line.strip().split(delimiter)          
-        
-            # skip first if first line of data is header
-            if(not is_columns_created):
-                if(header):
-                    columns = create_col(row,clean_chars=clean_chars_header)                                        
+            # split the line by delimiter
+            row = line.strip().split(delimiter)
+            # check if the columns were created or not
+            if (not is_columns_created):
+                # create columns by using header if the file has a header
+                if (header):
+                    # create columns with header
+                    columns = create_col(row, clean_chars=clean_chars_header)
+                    # switch "true" if columns could be created properly
+                    is_columns_created = True
+                    # skip first line if it is header
+                    continue
                 else:
-                    columns = create_col(col_count=len(row),clean_chars=clean_chars_header)
-                
-                is_columns_created = True
-                continue                
-                
-            if len(row)!=len(columns):
-                split_error_count += 1                
+                    # create default columns if the file has not a header
+                    columns = create_col(col_count=len(row), clean_chars=clean_chars_header)
+                    # switch "true" if columns could be created properly
+                    is_columns_created = True
+            # the line was not split properly since it may have some the same delimiter char which is being used for
+            # splitting inside the item
+            if len(row) != len(columns):
+                # increment the split error counter
+                split_error_count += 1
+                # assign the last failure line
                 sample_split_error = line
             else:
+                # create a empty dictionary row
                 row_dict = {}
-                for idx,col_value in enumerate(row):
-                    row_dict[columns[idx]] = col_value                    
+                # build dictionary with key:header_name, value: line_cell
+                for idx, col_value in enumerate(row):
+                    row_dict[columns[idx]] = col_value
+                # append the dictionary to result list
                 result.append(row_dict)
-    
-    if(split_error_count>0):
-        print (f"[{split_error_count}] rows could not be splitted. Sample row : [{sample_split_error}] ")
-           
+    # display the line which was split wrong to help fixing the issue for user
+    # user may change delimiter or fix some cell
+    if (split_error_count > 0):
+        print(f"[{split_error_count}] rows could not be splitted. Sample row : [{sample_split_error}] ")
+    # return result list which contains some dictionary items
     return result
 
 
 def select_columns(data, cols):
     selected_columns_data_set = []
-    
+
     for row in data:
         selected_columns_data_set.append(build_row_by_columns(row, cols))
-    
-    return selected_columns_data_set    
+
+    return selected_columns_data_set
 
 
-def build_row_by_columns(data_item, cols):               
+def build_row_by_columns(data_item, cols):
     row_dict = {}
     for select_col in cols:
         row_dict[select_col] = data_item[select_col]
-    return row_dict    
-    
-        
-def sample_rows(data,count):
+    return row_dict
+
+
+def sample_rows(data, count):
     return data[:count]
 
 
 def get_columns(data):
-    return [item for item in data[0]] 
+    return [item for item in data[0]]
 
-        
+
 def create_col(header=None, col_count=None, clean_chars=[]):
     col_names = []
-    
-    if(header):
+
+    if (header):
         for col_name in header:
             col_names.append(create_column_name(col_names, col_name, clean_chars))
-    elif(col_count):
-        col_names = [f"col_{i+1}" for i in range(col_count)]
-    
+    elif (col_count):
+        col_names = [f"col_{i + 1}" for i in range(col_count)]
+
     return col_names
-            
+
 
 def create_column_name(col_names, value, clean_chars):
     if value:
         for sc in clean_chars:
-            value = ''.join(value.split()).replace(sc,'')
+            value = ''.join(value.split()).replace(sc, '')
         else:
             value = ''.join(value.split())
-        
+
     col_idx = 1
-    
+
     while True:
         if value in col_names:
             value = f"{value}_{col_idx}"
-            col_idx += 1 
+            col_idx += 1
         else:
-            return value    
+            return value
 
-def filter_by(data,filter_values):
+
+def filter_by(data, filter_values):
     filtered_list = data
-    
+
     for filter_col in filter_values:
         filtered_list = list(filter(lambda data: data[filter_col] in filter_values[filter_col], filtered_list))
-        
+
     return filtered_list
+
 
 def group_by(data, group_columns, show_columns, aggregation):
     if not aggregation in ['count']:
         print(f'[{aggregation}] is not supported')
         return data
-    
-    group_column_name = aggregation+"_"+"_".join(group_columns)
-    
+
+    group_column_name = aggregation + "_" + "_".join(group_columns)
+
     result = []
-    
-    if aggregation=="count":
+
+    if aggregation == "count":
         result = aggregation_count(data, group_column_name, group_columns, show_columns)
-    
+
     show_columns.append(group_column_name)
-                          
-    return select_columns(result, show_columns)    
+
+    return select_columns(result, show_columns)
+
 
 def aggregation_count(data, group_column_name, group_columns, show_columns):
     group_columns_dict = {}
     data_group_by_list = []
-    
+
     for item in data:
         # dictionary item 
         group_by_dict_key = tuple([item[key] for key in item if key in group_columns])
         data_group_by_list_filtered = list(filter(lambda data: group_by_dict_key in data, data_group_by_list))
-        
-        if len(data_group_by_list_filtered)==0:                
+
+        if len(data_group_by_list_filtered) == 0:
             group_columns_dict = build_row_by_columns(item, show_columns)
             group_columns_dict[group_column_name] = 1
             group_columns_dict[group_by_dict_key] = 1
             data_group_by_list.append(group_columns_dict)
-        else:    
+        else:
             for item in data_group_by_list_filtered:
                 item[group_column_name] = item[group_column_name] + 1
                 item[group_by_dict_key] = item[group_by_dict_key] + 1
-     
-    return data_group_by_list           
-                    
- 
-def create_data_set(data, key_column, data_set_column ):
+
+    return data_group_by_list
+
+
+def create_data_set(data, key_column, data_set_column):
     result = []
-    
+
     for item in data:
         result_item_key = item[key_column]
-        result_item_data_set_value = item[data_set_column] 
-        
+        result_item_data_set_value = item[data_set_column]
+
         result_filtered = list(filter(lambda data: result_item_key in data, result))
-        
-        if len(result_filtered)==0:
-            if(len(result)==0):
-                result = [{ result_item_key: [ result_item_data_set_value ] }]
+
+        if len(result_filtered) == 0:
+            if (len(result) == 0):
+                result = [{result_item_key: [result_item_data_set_value]}]
             else:
-                result.append({ result_item_key: [ result_item_data_set_value ] })
+                result.append({result_item_key: [result_item_data_set_value]})
         else:
             for result_item in result_filtered:
                 result_item_values = result_item[result_item_key]
                 result_item_values.append(result_item_data_set_value)
-        
-                
-    return result    
-       
-if __name__ == "__main__" :
-    data_set = read_csv("/home/fsusam/Development/workspace/python/standard-deviation/src/examples/data/athlete_events.csv",clean_chars_header=['"'])
+
+    return result
+
+
+if __name__ == "__main__":
+    data_set = read_csv(
+        "/home/fsusam/Development/workspace/python/standard-deviation/src/examples/data/athlete_events.csv",
+        clean_chars_header=['"'])
     print()
-    print(f"Sample Row : {sample_rows(data_set,1)}" )
-    
-    data_set = select_columns(data_set, ['NOC', 'Year', 'Medal'] )
+    print(f"Sample Row : {sample_rows(data_set, 1)}")
+
+    data_set = select_columns(data_set, ['NOC', 'Year', 'Medal'])
     print()
-    print(f"Sample Row selected columns : {sample_rows(data_set,1)}" )
-    
+    print(f"Sample Row selected columns : {sample_rows(data_set, 1)}")
+
     print()
     columns = get_columns(data_set)
     print(f"Columns : {columns}")
-    
+
     print()
-    data_set = filter_by(data_set, {"NOC":['"CHN"','"GER"'] , "Medal":['"Silver"','"Bronze"','"Gold"']} )
-    print(f"Sample row filtered data :{sample_rows(data_set,1)}")
-    
+    data_set = filter_by(data_set, {"NOC": ['"CHN"', '"GER"'], "Medal": ['"Silver"', '"Bronze"', '"Gold"']})
+    print(f"Sample row filtered data :{sample_rows(data_set, 1)}")
+
     print()
-    data_set = group_by(data_set, group_columns=["NOC", "Year"] , show_columns=["NOC", "Year"], aggregation="count")
-    print(f"Sample row group_by data :{sample_rows(data_set,1)}")
-    
+    data_set = group_by(data_set, group_columns=["NOC", "Year"], show_columns=["NOC", "Year"], aggregation="count")
+    print(f"Sample row group_by data :{sample_rows(data_set, 1)}")
+
     print()
     data_set_plot = create_data_set(data_set, "NOC", "count_NOC_Year")
     print(f"Sample row data_set_plot :{data_set_plot}")
- 
--- src/statistical_data_analysis_tool.py
+++ src/statistical_data_analysis_tool.py
@@ -7,290 +7,290 @@ import math
 
 # a measure of the amount of variation or dispersion of a set of values
 def standard_deviation(data_set):
-       """
-       Calculate standard deviation
+    """
+    Calculate standard deviation
 
-       Parameters
-       ----------
-       data_set : list
-              consist of numeric values. eg: [1,2,3,4,5]
-      
-       Returns
-       -------
-       float
-              standard deviation.
-       """
-       # Calculate the Standard Deviation
-       return math.sqrt(variance(data_set))
+    Parameters
+    ----------
+    data_set : list
+           consist of numeric values. eg: [1,2,3,4,5]
+
+    Returns
+    -------
+    float
+           standard deviation.
+    """
+    # Calculate the Standard Deviation
+    return math.sqrt(variance(data_set))
 
 
 # average of the data set
 def mean(data_set):
-       """
-       Calculate avarage of data set
+    """
+    Calculate avarage of data set
 
-       Parameters
-       ----------
-       data_set : list
-              consist of numeric values. eg: [1,2,3,4,5]
-      
-       Returns
-       -------
-       float
-              mean
-       """
-       # verify data length
-       verify_data_set_length(expected_length_at_least=1, data_set=data_set, func_name='mean')
-    
-       # Calculate mean
-       return sum(data_set)/len(data_set)
+    Parameters
+    ----------
+    data_set : list
+           consist of numeric values. eg: [1,2,3,4,5]
+
+    Returns
+    -------
+    float
+           mean
+    """
+    # verify data length
+    verify_data_set_length(expected_length_at_least=1, data_set=data_set, func_name='mean')
+
+    # Calculate mean
+    return sum(data_set) / len(data_set)
 
 
 # how far a set of (random) numbers are spread out from their average value
 def variance(data_set):
-       """
-       Calculate how far a set of (random) numbers are spread out from their average value
+    """
+    Calculate how far a set of (random) numbers are spread out from their average value
 
-       Parameters
-       ----------
-       data_set : list
-              consist of numeric values. eg: [1,2,3,4,5]
-      
-       Returns
-       -------
-       float
-              variance
-       """
-       # verify data length
-       verify_data_set_length(expected_length_at_least=2, data_set=data_set, func_name='variance')
-       
-       # call mean function to find mean of data set
-       average=mean(data_set)
-       # find the Variance
-       return sum([abs((x-average)**2) for x in data_set]) / (len(data_set)-1)
+    Parameters
+    ----------
+    data_set : list
+           consist of numeric values. eg: [1,2,3,4,5]
+
+    Returns
+    -------
+    float
+           variance
+    """
+    # verify data length
+    verify_data_set_length(expected_length_at_least=2, data_set=data_set, func_name='variance')
+
+    # call mean function to find mean of data set
+    average = mean(data_set)
+    # find the Variance
+    return sum([abs((x - average) ** 2) for x in data_set]) / (len(data_set) - 1)
 
 
 # middle number in a data set
 def median(data_set):
-       """
-       find the middle number in a data set
+    """
+    find the middle number in a data set
 
-       Parameters
-       ----------
-       data_set : list
-              consist of numeric values. eg: [1,2,3,4,5]
-      
-       Returns
-       -------
-       float
-              median
-       """
-       # verify data length
-       verify_data_set_length(expected_length_at_least=1, data_set=data_set, func_name='median')
-    
-       # sort the data set
-       data_set.sort()
-    
-       return get_item_middle_data_set(data_set)
+    Parameters
+    ----------
+    data_set : list
+           consist of numeric values. eg: [1,2,3,4,5]
+
+    Returns
+    -------
+    float
+           median
+    """
+    # verify data length
+    verify_data_set_length(expected_length_at_least=1, data_set=data_set, func_name='median')
+
+    # sort the data set
+    data_set.sort()
+
+    return get_item_middle_data_set(data_set)
 
 
 # calculate correlation
 # measure of the linear relationship between two quantitative variables    
 def correlation_pearson(data_setx, data_sety):
-       """
-       Calculate measure of the linear relationship between two quantitative variables
+    """
+    Calculate measure of the linear relationship between two quantitative variables
 
-       Parameters
-       ----------
-       data_set : list
-              consist of numeric values. eg: [1,2,3,4,5]
-      
-       Returns
-       -------
-       float
-              pearsonr
-       """
-       # verify data length
-       verify_data_set_length(expected_length_at_least=2, data_set=data_setx, func_name='correlation_pearson')
-       
-       # verify data length
-       verify_data_set_length(expected_length_at_least=2, data_set=data_sety, func_name='correlation_pearson')
-       
-       # data set should have the same length
-       if len(data_setx)!=len(data_sety) :
-              raise ValueError("data_setx and data_sety must have the same length.")
-       
-       # one of data set length
-       n= len(data_setx)
-    
-       # merge data sets and convert to tuple
-       tuple_xy = tuple(zip(data_setx,data_sety))
-       # multiply by each other every x and y items which comes from tuple and multiply by length     
-       n_sum_tuple = n*sum([x*y for x,y in tuple_xy])
-       # sum x and y data set
-       sum_x = sum(data_setx)
-       sum_y = sum(data_sety)
+    Parameters
+    ----------
+    data_set : list
+           consist of numeric values. eg: [1,2,3,4,5]
 
-       # sum pow(x,2) and multiply by n
-       n_sum_x_pow2 = n*sum([x**2 for x in data_setx])
-       n_sum_y_pow2 = n*sum([y**2 for y in data_sety])
-    
-       # sum pow(sum(x),2)
-       sum_x_pow2 = sum_x**2
-       sum_y_pow2 = sum_y**2
-    
-       # calculate pearson correlation
-       result = (n_sum_tuple - (sum_x * sum_y)) / math.sqrt((n_sum_x_pow2 - sum_x_pow2) * (n_sum_y_pow2 - sum_y_pow2))
-    
-       return result      
+    Returns
+    -------
+    float
+           pearsonr
+    """
+    # verify data length
+    verify_data_set_length(expected_length_at_least=2, data_set=data_setx, func_name='correlation_pearson')
+
+    # verify data length
+    verify_data_set_length(expected_length_at_least=2, data_set=data_sety, func_name='correlation_pearson')
+
+    # data set should have the same length
+    if len(data_setx) != len(data_sety):
+        raise ValueError("data_setx and data_sety must have the same length.")
+
+    # one of data set length
+    n = len(data_setx)
+
+    # merge data sets and convert to tuple
+    tuple_xy = tuple(zip(data_setx, data_sety))
+    # multiply by each other every x and y items which comes from tuple and multiply by length
+    n_sum_tuple = n * sum([x * y for x, y in tuple_xy])
+    # sum x and y data set
+    sum_x = sum(data_setx)
+    sum_y = sum(data_sety)
+
+    # sum pow(x,2) and multiply by n
+    n_sum_x_pow2 = n * sum([x ** 2 for x in data_setx])
+    n_sum_y_pow2 = n * sum([y ** 2 for y in data_sety])
+
+    # sum pow(sum(x),2)
+    sum_x_pow2 = sum_x ** 2
+    sum_y_pow2 = sum_y ** 2
+
+    # calculate pearson correlation
+    result = (n_sum_tuple - (sum_x * sum_y)) / math.sqrt((n_sum_x_pow2 - sum_x_pow2) * (n_sum_y_pow2 - sum_y_pow2))
+
+    return result
 
 
 # the middle number between the smallest number and the median of the data set.                        
 def quartile_first(data_set):
-       """
-       Find the middle number between the smallest number and the median of the data set
+    """
+    Find the middle number between the smallest number and the median of the data set
 
-       Parameters
-       ----------
-       data_set : list
-              consist of numeric values. eg: [1,2,3,4,5]
-      
-       Returns
-       -------
-       float
-              quartile (Q1)
-       """
-       verify_data_set_length(expected_length_at_least=1, data_set=data_set, func_name='quartile_first')
-       
-       # sort the data set
-       data_set.sort()
-    
-       # find the mid-index
-       mid_index = int(len(data_set)/2)
-    
-       return get_item_middle_data_set(data_set[0:mid_index+1])
+    Parameters
+    ----------
+    data_set : list
+           consist of numeric values. eg: [1,2,3,4,5]
+
+    Returns
+    -------
+    float
+           quartile (Q1)
+    """
+    verify_data_set_length(expected_length_at_least=1, data_set=data_set, func_name='quartile_first')
+
+    # sort the data set
+    data_set.sort()
+
+    # find the mid-index
+    mid_index = int(len(data_set) / 2)
+
+    return get_item_middle_data_set(data_set[0:mid_index + 1])
 
 
 # the middle value between the median and the highest value of the data set
 def quartile_third(data_set):
-       """
-       Find the middle value between the median and the highest value of the data set
+    """
+    Find the middle value between the median and the highest value of the data set
 
-       Parameters
-       ----------
-       data_set : list
-              consist of numeric values. eg: [1,2,3,4,5]
-      
-       Returns
-       -------
-       float
-              quartile (Q3)
-       """
-       # verify data length
-       verify_data_set_length(expected_length_at_least=1, data_set=data_set, func_name='quartile_third')
-       
-       # sort the data set
-       data_set.sort()
-    
-       # find the mid-index
-       mid_index = int(len(data_set)/2)
-    
-       if len(data_set) % 2:
-              # single number in the middle of the data set
-              return get_item_middle_data_set(data_set[mid_index:])
-       else:
-              # two number in the middle of the data set
-              return get_item_middle_data_set(data_set[mid_index-1:])
- 
-       
+    Parameters
+    ----------
+    data_set : list
+           consist of numeric values. eg: [1,2,3,4,5]
+
+    Returns
+    -------
+    float
+           quartile (Q3)
+    """
+    # verify data length
+    verify_data_set_length(expected_length_at_least=1, data_set=data_set, func_name='quartile_third')
+
+    # sort the data set
+    data_set.sort()
+
+    # find the mid-index
+    mid_index = int(len(data_set) / 2)
+
+    if len(data_set) % 2:
+        # single number in the middle of the data set
+        return get_item_middle_data_set(data_set[mid_index:])
+    else:
+        # two number in the middle of the data set
+        return get_item_middle_data_set(data_set[mid_index - 1:])
+
+
 # min value of data set
 def min_data_set(data_set):
-       """
-       min value of data set
+    """
+    min value of data set
 
-       Parameters
-       ----------
-       data_set : list
-              consist of numeric values. eg: [1,2,3,4,5]
-      
-       Returns
-       -------
-       Numeric
-              min item in the data set
-       """
-       # verify data length
-       verify_data_set_length(expected_length_at_least=1, data_set=data_set, func_name='min_data_set')
-       
-       return min(data_set)
+    Parameters
+    ----------
+    data_set : list
+           consist of numeric values. eg: [1,2,3,4,5]
+
+    Returns
+    -------
+    Numeric
+           min item in the data set
+    """
+    # verify data length
+    verify_data_set_length(expected_length_at_least=1, data_set=data_set, func_name='min_data_set')
+
+    return min(data_set)
 
 
 # max value of data set
 def max_data_set(data_set):
-       """
-       max value of data set
+    """
+    max value of data set
 
-       Parameters
-       ----------
-       data_set : list
-              consist of numeric values. eg: [1,2,3,4,5]
-      
-       Returns
-       -------
-       float
-              max item in the data_set
-       """
-       # verify data length
-       verify_data_set_length(expected_length_at_least=1, data_set=data_set, func_name='max_data_set')
-    
-       return max(data_set)
+    Parameters
+    ----------
+    data_set : list
+           consist of numeric values. eg: [1,2,3,4,5]
+
+    Returns
+    -------
+    float
+           max item in the data_set
+    """
+    # verify data length
+    verify_data_set_length(expected_length_at_least=1, data_set=data_set, func_name='max_data_set')
+
+    return max(data_set)
 
 
 # get the item which is the middle of the data set
 def get_item_middle_data_set(data_set):
-       """
-       Find the middle index of the data set and decide the middle of the data set is single or not
+    """
+    Find the middle index of the data set and decide the middle of the data set is single or not
 
-       Parameters
-       ----------
-       data_set : list
-              consist of numeric values. eg: [1,2,3,4,5]
-      
-       Returns
-       -------
-       float
-              item which is the middle of the data set
-       """   
-       # find the mid-index
-       mid_index = int(len(data_set)/2)
-    
-       if len(data_set) % 2:
-              # single number in the middle of the data set
-              return data_set[mid_index]                
-       else: 
-              # two number in the middle of the data set
-              return (data_set[mid_index-1] + data_set[mid_index])/2
+    Parameters
+    ----------
+    data_set : list
+           consist of numeric values. eg: [1,2,3,4,5]
+
+    Returns
+    -------
+    float
+           item which is the middle of the data set
+    """
+    # find the mid-index
+    mid_index = int(len(data_set) / 2)
+
+    if len(data_set) % 2:
+        # single number in the middle of the data set
+        return data_set[mid_index]
+    else:
+        # two number in the middle of the data set
+        return (data_set[mid_index - 1] + data_set[mid_index]) / 2
 
 
 # common data set length check
 def verify_data_set_length(expected_length_at_least, data_set, func_name):
-       """
-       Verify the data set length by specified data set length
+    """
+    Verify the data set length by specified data set length
 
-       Parameters
-       ----------
-       data_set : list
-              consist of numeric values. eg: [1,2,3,4,5]
-       
-       expected_length_at_least : int
-              the smallest data point. eg: it can't be >>> len(data_set)<expected_length_at_least
-       
-       func_name : str
-              invoked function name. eg: "variance" or "mean"       
-       
-       Raise
-       -------
-       Exception
-              Raise Exception if len(data_set)<expected_length_at_least
-       """
-       if len(data_set)<expected_length_at_least :
-           raise Exception(f"{func_name} requires at least {expected_length_at_least} data points")
+    Parameters
+    ----------
+    data_set : list
+           list of values. eg: [1,2,3,4,5]
+
+    expected_length_at_least : int
+           the smallest data point. The data set length can not be less then "expected_length_at_least"
+
+    func_name : str
+           invoked function name. eg: "variance" or "mean"
+
+    Raise
+    -------
+    Exception
+           Raise Exception if len(data_set)<expected_length_at_least
+    """
+    if len(data_set) < expected_length_at_least:
+        raise Exception(f"{func_name} requires at least {expected_length_at_least} data points")
